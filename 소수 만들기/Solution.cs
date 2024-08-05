a11a520b86eabe38eb7290aca778ff5470a209c4
JeeSeong <rhtmxm17@gmail.com>
2024-08-05 18:15:27 +0900

    소수 만들기/임시 코드 삭제 및 개선점 적용

--- 소수 만들기/Solution.cs
+++ 소수 만들기/Solution.cs
@@ -2,23 +2,15 @@
 {
     internal class Solution
     {
-        private List<int> primes = new List<int>() { 2 };
-        private int checkedRange = 2;
-
-        private void PrimeCheckRange(int max)
-        {
-            for (int i = checkedRange + 1; i <= max; i++)
-            {
-
-            }
-        }
-
+        // 입력값 범위를 넘어서면 잘못된 결과 발생 가능
+        private int[] primes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, int.MaxValue };
+        
         private bool IsPrime(int value)
         {
             int range = (int)Math.Sqrt(value);
-            for (int i = 2; i <= range; i++)
+            for (int i = 0; primes[i] <= range; i++)
             {
-                if (value % i == 0)
+                if (value % primes[i] == 0)
                     return false;
             }
             return true;
@@ -28,9 +20,6 @@
         {
             int answer = 0;
 
-            //int cases = nums.Length * (nums.Length - 1) * (nums.Length - 2);
-            //int[] sum = new int[cases];
-            //int index = 0;
             for (int i = 0; i < nums.Length; i++)
             {
                 for (int j = i + 1; j < nums.Length; j++)
