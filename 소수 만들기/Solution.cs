29f697a92ab7fe8c72f234d1ebd186ab706badf9
JeeSeong <rhtmxm17@gmail.com>
2024-08-05 17:39:10 +0900

    소수 만들기
    프로그래머스 코딩 테스트
    https://school.programmers.co.kr/learn/courses/30/lessons/12977

---
+++ 소수 만들기/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace 소수_만들기
+{
+    internal class Solution
+    {
+        private List<int> primes = new List<int>() { 2 };
+        private int checkedRange = 2;
+
+        private void PrimeCheckRange(int max)
+        {
+            for (int i = checkedRange + 1; i <= max; i++)
+            {
+
+            }
+        }
+
+        private bool IsPrime(int value)
+        {
+            int range = (int)Math.Sqrt(value);
+            for (int i = 2; i <= range; i++)
+            {
+                if (value % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public int solution(int[] nums)
+        {
+            int answer = 0;
+
+            //int cases = nums.Length * (nums.Length - 1) * (nums.Length - 2);
+            //int[] sum = new int[cases];
+            //int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (IsPrime(nums[i] + nums[j] + nums[k]))
+                            answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ 소수 만들기/Tester.cs
@@ -0,0 +1,11 @@
+﻿namespace 소수_만들기
+{
+    internal class Tester
+    {
+        static void Main()
+        {
+            Solution sol = new();
+            Console.Write(sol.solution(new int[] { 1, 2, 3, 4 }));
+        }
+    }
+}
