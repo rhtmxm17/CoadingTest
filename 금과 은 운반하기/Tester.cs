c4a49d537c0caa5761aaaaa2b5886b1803d1bc7e
JeeSeong <rhtmxm17@gmail.com>
2024-08-06 22:05:15 +0900

    금과 은 운반하기

---
+++ 금과 은 운반하기/Tester.cs
@@ -0,0 +1,9 @@
+﻿public class Tester
+{
+    static void Main()
+    {
+        Solution sol = new();
+        Console.WriteLine(sol.solution(10, 10, [100], [100], [7], [10])); // 50
+        Console.WriteLine(sol.solution(90, 500, [70, 70, 0], [0, 0, 500], [100, 100, 2], [4, 8, 1])); // 499
+    }
+}
