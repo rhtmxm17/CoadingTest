51f651b815fb9fc76d27352e61647160fe4f4e63
JeeSeong <rhtmxm17@gmail.com>
2024-08-07 18:16:01 +0900

    올바른 괄호의 갯수

---
+++ 올바른 괄호의 갯수/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public long solution(int n)
+    {
+        long factoN = 1;
+        long Nto2N = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factoN *= i;
+        }
+
+        for (int i = n + 2;i <= 2 * n; i++)
+        {
+            Nto2N *= i;
+        }
+
+        return (long)(Nto2N / factoN);
+    }
+}
---
+++ 올바른 괄호의 갯수/Tester.cs
@@ -0,0 +1,21 @@
+﻿public class Tester
+{
+    static void Main()
+    {
+        Solution sol = new();
+        Console.WriteLine(sol.solution(1));
+        Console.WriteLine(sol.solution(3)); // 5
+        Console.WriteLine(sol.solution(4)); // 14
+        Console.WriteLine(sol.solution(5)); // 42
+        Console.WriteLine(sol.solution(6));
+        Console.WriteLine(sol.solution(7));
+        Console.WriteLine(sol.solution(8));
+        Console.WriteLine(sol.solution(9));
+        Console.WriteLine(sol.solution(10));
+        Console.WriteLine(sol.solution(11));
+        Console.WriteLine(sol.solution(12));
+        Console.WriteLine(sol.solution(13));
+        Console.WriteLine(sol.solution(14));
+        Console.WriteLine(sol.solution(15));
+    }
+}
