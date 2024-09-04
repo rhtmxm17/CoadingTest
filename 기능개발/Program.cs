55525272f8f04e8aab759fbd7a6319401530e625
JeeSeong <rhtmxm17@gmail.com>
2024-09-04 21:54:54 +0900

    기능개발

---
+++ 기능개발/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] progresses, int[] speeds)
+    {
+        if (progresses.Length == 0)
+            return Array.Empty<int>();
+
+        List<int> answer = new List<int>(progresses.Length);
+        int dayElapsed = 0;
+        int lastCompletedIndex = 0;
+
+        // 첫번째 작업 소요 일 수
+        int todo = (100 - progresses[0]);
+        dayElapsed = todo / speeds[0] + (todo % speeds[0] > 0 ? 1 : 0);
+        for (int i = 1; i < progresses.Length; i++)
+        {
+            todo = (100 - progresses[i]);
+            int dayspan = todo / speeds[i] + (todo % speeds[i] > 0 ? 1 : 0);
+            if (dayspan <= dayElapsed) // 이미 완료된 작업이면 다음 작업 확인
+                continue;
+
+            dayElapsed = dayspan; // 배포된 날짜 누적
+
+            answer.Add(i - lastCompletedIndex);
+            lastCompletedIndex = i;
+        }
+        answer.Add(progresses.Length - lastCompletedIndex);
+
+        return answer.ToArray();
+    }
+}
---
+++ 기능개발/Tester.cs
@@ -0,0 +1,9 @@
+﻿internal class Tester
+{
+    static void Main()
+    {
+        Solution sol = new();
+        System.Console.WriteLine(string.Join(',', sol.solution([93, 30, 55], [1, 30, 5])));
+        System.Console.WriteLine(string.Join(',', sol.solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])));
+    }
+}
